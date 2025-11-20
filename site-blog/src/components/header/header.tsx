4e59ae4e775a11b26db8ef0fb01dac61b7e8686a
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-11-20 16:54:37 -0300

    chore(20-11-2025): Adicionando imagem log ao Componente Header

--- site-blog/src/components/header/header.tsx
+++ site-blog/src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Button } from "../ui/button";
@@ -12,7 +13,9 @@ export const Header = () => {
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-backgrouund/60 ">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
+          <Link href="/">
+            <Image src="/Brand-Logo.svg" alt="Logo" width={120} height={40} />
+          </Link>
 
           <nav className="flex items-center gap-6">
             <Link
