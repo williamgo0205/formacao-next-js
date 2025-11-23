0937babbb445b4153b93f19350250ecf99a15f86
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-11-23 12:04:31 -0300

    chore(23-11-2025): Adicionando componente active-link

---
+++ site-blog/src/components/active-link/active-link.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+
+type ActiveLinkProps = {
+  children: React.ReactNode;
+} & LinkProps;
+
+export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
+  const router = useRouter();
+  const isCurrentPath =
+    router.asPath === href ||
+    router.asPath === rest.as ||
+    router.asPath.startsWith(String(rest.as));
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "text-sm font-medium transition-colors hover:text-blue-500",
+        isCurrentPath ? "text-blue-500" : "text-muted-foreground"
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
--- site-blog/src/components/header/header.tsx
+++ site-blog/src/components/header/header.tsx
@@ -1,14 +1,9 @@
-import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { ActiveLink } from "../active-link";
 import { Button } from "../ui/button";
 
 export const Header = () => {
-  const router = useRouter();
-  const isHomePage = router.pathname === "/";
-  const isBlogPage = router.pathname.startsWith("/blog");
-
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-backgrouund/60 ">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -18,25 +13,11 @@ export const Header = () => {
           </Link>
 
           <nav className="flex items-center gap-6">
-            <Link
-              href="/"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isHomePage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Início
-            </Link>
-            <Link
-              href="/blog"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isBlogPage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Blog
-            </Link>
-            <Button variant="secondary"> Começar</Button>
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Começar</Link>
+            </Button>
           </nav>
         </div>
       </div>
