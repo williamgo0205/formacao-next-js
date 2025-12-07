cf1617427a85343317d31cd4e6bb4456d6329275
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-12-07 14:38:57 -0300

    chore(07-12-2025): Adicionando componente Hero

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,20 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function HomePage() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.variable} 
-              grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family:var(font-geist-sans)]`}
-    />
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
