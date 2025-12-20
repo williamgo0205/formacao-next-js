5e91c385f7a21e7959fd2f24277ee35cc20de787
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-12-20 15:51:30 -0300

    chore(20-12-2025): Adicionando componente Customer Story

--- site-blog/src/components/support-section/support-section.tsx
+++ site-blog/src/components/support-section/support-section.tsx
@@ -8,7 +8,7 @@ const ptSansCaption = PT_Sans_Caption({
 
 export const SupportSection = () => {
   return (
-    <section className="pb-8 md:py-10 bg-gray-700">
+    <section className="py-12 md:py-30 bg-gray-700">
       <div className="container flex flex-col items-center gap-12">
         <h2 className={`${ptSansCaption.className} text-balance text-center text-heading-xl text-gray-100`}>Sua loja de afiliados, simples, <br /> do jeito que deveria ser</h2>
 
