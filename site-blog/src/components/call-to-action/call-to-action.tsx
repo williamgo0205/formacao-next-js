50882c8624905bb8db9cc1d19647fcba8548a068
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-12-20 16:58:24 -0300

    chore(20-12-2025): Adicionando refactory para ajustes seções com imagens

--- site-blog/src/components/call-to-action/call-to-action.tsx
+++ site-blog/src/components/call-to-action/call-to-action.tsx
@@ -4,9 +4,10 @@ import { Button } from "../ui/button";
 
 export const CallToAction = () => {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 hidden md:block bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
 
-      <div className="container">
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
           <div className="p-4 bg-cyan-300 w-fit rounded-full">
             <Store className="text-cyan-100" />
--- site-blog/src/components/support-section/support-section.tsx
+++ site-blog/src/components/support-section/support-section.tsx
@@ -3,8 +3,10 @@ import { HeartHandshake, PaintbrushVertical, Store } from "lucide-react"
 
 export const SupportSection = () => {
   return (
-    <section className="py-12 md:py-30 bg-gray-700">
-      <div className="container flex flex-col items-center gap-12">
+    <section className="relative pb-8 md:py-10 bg-gray-700">
+      <div className="absolute inset-0 hidden md:block bg-[url('/background-features.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+
+      <div className="container flex flex-col items-center gap-12 relative">
         <h2 className="font-sans text-balance text-center text-heading-xl text-gray-100">Sua loja de afiliados, simples, <br /> do jeito que deveria ser</h2>
 
         <div className="grid gap-6 md:grid-cols-3">
