a6acb422b26adea2af173fa34a7d635005104ccd
William Goncalves de Oliveira <williamgo0205@gmail.com>
2025-12-28 15:13:05 -0300

    chore(28-12-2025): Adicionando Primeiro post em Markdown

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { LandingPage } from "@/templates/landing-page";
 
+import { allPosts } from 'contentlayer/generated';
+
 export default function HomePage() {
+  console.log(allPosts)
   return (
     <LandingPage />
   );
