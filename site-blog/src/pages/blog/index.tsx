61870f9c35aa033dbe77eb6d1f7425186ba4b40b
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-01-03 15:10:12 -0300

    chore(03-01-2026): Adicionando Página Blog

--- site-blog/src/pages/blog/index.tsx
+++ site-blog/src/pages/blog/index.tsx
@@ -1,8 +1,27 @@
 
 export default function BlogPage() {
   return (
-    <>
-      <h2>Blog</h2>
-    </>
+    <div className="flex flex-col py-24 flex-grow h-full">
+      <header className="">
+        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            {/* TAG */}
+            <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
+              BLOG
+            </span>
+
+            {/* Título  */}
+            <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">
+              Dicas e estratégias para impulsionar seu negócio
+            </h1>
+          </div>
+        </div>
+      </header>
+
+
+      {/* Search */}
+
+      {/* Listagens de Posts*/}
+    </div>
   );
 }
--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { LandingPage } from "@/templates/landing-page";
 
-import { allPosts } from 'contentlayer/generated';
 
 export default function HomePage() {
-  console.log(allPosts)
   return (
     <LandingPage />
   );
