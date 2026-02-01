7b035339a586f0e8b912ba8b82f76a26ff93fc40
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-02-01 12:37:29 -0300

    chore(01-02-2026): Adicionando mudanças no código de PAGES ROUTER para  APP ROUTER na migração da landing page

--- site-blog/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { LandingPage } from "@/templates/landing-page";
-
-
-export default function HomePage() {
-  return (
-    <LandingPage />
-  );
-}
--- site-blog/src/templates/landing-page/sections/hero-section/hero-section.tsx
+++ site-blog/src/templates/landing-page/sections/hero-section/hero-section.tsx
@@ -3,11 +3,10 @@ import { ArrowRight, Clock, Store } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
-
 export const HeroSection = () => {
   return (
     <section className="container relative flex items-center justify-center mt-16">
-      <div className="grid grid-cols-1 md:grid-cols-2 gag-8 min-h-[20rem] md:h-[36rem] items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 min-h-[20rem] md:h-[36rem] items-center">
         <div className="flex flex-col itens-center justify-center gap-4 md:items-start lg:items-start">
           <h1 className="text-gray-100 text-heading-hg font-sans">Venda seus produtos como afiliado em um único lugar</h1>
 
