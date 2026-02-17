5d817988fe0e14c9c61f70aaafe800b5829f5fe4
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-02-17 16:46:24 -0300

    chore(17-02-2026): Adicionando metatags para blog page list e layout

--- site-blog/src/app/blog/page.tsx
+++ site-blog/src/app/blog/page.tsx
@@ -1,5 +1,29 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
+
+// Adicionando as metatags para o projeto
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para ipulsionar seu negocio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para ipulsionar seu negocio',
+    url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set'
+      }
+    ]
+  }
+}
 
 // Componente poir default é criado como server client
 export default function BlogListPages() {
