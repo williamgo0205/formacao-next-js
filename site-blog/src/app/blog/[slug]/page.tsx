8018f40b0298c50b0e41ea1de53d9e963e4b5f61
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-02-17 16:56:31 -0300

    chore(17-02-2026): Adicionando metatags  slug de forma dinâmica para cada post

--- site-blog/src/app/blog/[slug]/page.tsx
+++ site-blog/src/app/blog/[slug]/page.tsx
@@ -1,11 +1,32 @@
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>
+}
+
+// Adicionando as metatags para o projeto de forma dinâmica para o post selecionado
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  };
 }
 
 // Transformando o componente em ISR (Incremental Static Regeneration) 
@@ -24,7 +45,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
--- site-blog/src/app/layout.tsx
+++ site-blog/src/app/layout.tsx
@@ -5,6 +5,23 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://rocketseat-nextjs-fundamentals.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.set'
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
