adebd20b5de1ceffd01f70e1a1261b1a5780b6ba
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-02-22 10:32:09 -0300

    chore: Adicionando arquivo teste lefthook

--- pet-shop/src/app/page.tsx
+++ pet-shop/src/app/page.tsx
@@ -1,8 +1,21 @@
+type Props = {
+  title: string;
+}
+
+const Component = ({ title }: Props) => {
+  console.log(title);
+  return (
+    <>
+      <h2>Component</h2>
+    </>
+  )
+}
 
 export default function Home() {
   return (
     <div>
       <h2>Rockeseat</h2>
+      <Component title={1} />
     </div>
   );
 }
