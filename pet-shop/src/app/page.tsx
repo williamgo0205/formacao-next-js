0d901c234516b575c9c2e215b80edb6826fcd72c
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-02-28 16:43:39 -0300

    chore(28-02-2026): Inicializando o componente Homedo sistema

--- pet-shop/src/app/page.tsx
+++ pet-shop/src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-blue-300">Rocketseat</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-2">Sua Agenda</h1>
+          <p className="text-paragraph-small text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
