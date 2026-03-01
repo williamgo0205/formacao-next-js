b080075564404c41524296e6f850518e91de69d1
William Goncalves de Oliveira <williamgo0205@gmail.com>
2026-03-01 15:43:57 -0300

    chore(28-02-2026): Construindo a lógica para os períodos

--- pet-shop/src/app/page.tsx
+++ pet-shop/src/app/page.tsx
@@ -1,13 +1,117 @@
+import { PeriodSection } from '@/components/period-section/period-section';
+import { Appointment as AppointmentPrisma } from '@/generated/prisma';
+import {
+  Appointment,
+  AppointmentPeriod,
+  AppointmentPeriodDay,
+} from '@/types/appointment';
+
+const appointments = [
+  {
+    id: '1',
+    petName: 'Rex',
+    description: 'Consulta',
+    tutorName: 'João',
+    phone: '1234567890',
+    scheduleAt: new Date('2025-08-17T10:00:00'),
+  },
+  {
+    id: '2',
+    petName: 'Mimi',
+    tutorName: 'Maria',
+    description: 'Banho',
+    phone: '1234567890',
+    scheduleAt: new Date('2025-08-17T11:00:00'),
+  },
+  {
+    id: '3',
+    petName: 'Nina',
+    tutorName: 'Natalia',
+    description: 'Consulta',
+    phone: '1234567890',
+    scheduleAt: new Date('2025-08-17T14:00:00'),
+  },
+  {
+    id: '4',
+    petName: 'Nina',
+    tutorName: 'Natalia',
+    description: 'Consulta',
+    phone: '1234567890',
+    scheduleAt: new Date('2025-08-17T19:00:00'),
+  },
+];
+
+const getPeriod = (hour: number): AppointmentPeriodDay => {
+  if (hour >= 9 && hour < 12) return 'morning';
+  if (hour >= 13 && hour < 18) return 'afternoon';
+  return 'evening';
+};
+
+function groupAppointmentByPeriod(
+  appointments: AppointmentPrisma[]
+): AppointmentPeriod[] {
+  const transformedAppointments: Appointment[] = appointments?.map((apt) => ({
+    ...apt,
+    time: apt.scheduleAt.toLocaleTimeString('pt-BR', {
+      hour: '2-digit',
+      minute: '2-digit',
+    }),
+    service: apt.description,
+    period: getPeriod(apt.scheduleAt.getHours()),
+  }));
+
+  const morningAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'morning'
+  );
+  const afternoonAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'afternoon'
+  );
+  const eveningAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'evening'
+  );
+
+  return [
+    {
+      title: 'Manhã',
+      type: 'morning',
+      timeRange: '09h-12h',
+      appointments: morningAppointments,
+    },
+    {
+      title: 'Tarde',
+      type: 'afternoon',
+      timeRange: '13h-18h',
+      appointments: afternoonAppointments,
+    },
+    {
+      title: 'Noite',
+      type: 'evening',
+      timeRange: '19h-21h',
+      appointments: eveningAppointments,
+    },
+  ];
+}
+
 export default function Home() {
+  const periods = groupAppointmentByPeriod(appointments);
+
   return (
     <div className="bg-background-primary p-6">
       <div className="flex items-center justify-between md:m-8">
         <div>
-          <h1 className="text-title text-content-primary mb-2">Sua Agenda</h1>
-          <p className="text-paragraph-small text-content-secondary">
+          <h1 className="text-title-size text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium-size text-content-secondary">
             Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
         </div>
+      </div>
+
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
       </div>
     </div>
   );
---
+++ pet-shop/src/components/period-section/period-section.tsx
@@ -0,0 +1,30 @@
+import { AppointmentPeriod } from '@/types/appointment';
+import { Cloudy, Moon, Sun } from 'lucide-react';
+
+type PeriodSectionProps = {
+  period: AppointmentPeriod;
+};
+
+const periodIcons = {
+  morning: <Sun className="text-accent-blue" />,
+  afternoon: <Cloudy className="text-accent-orange" />,
+  evening: <Moon className="text-accent-yellow" />,
+};
+
+export const PeriodSection = ({ period }: PeriodSectionProps) => {
+  return (
+    <section className="mb-8 bg-background-tertiary rounded-xl">
+      <div className="flex items-center px-5 py-3 justify-between border-b border-[#2E2C30]">
+        <div className="flex items-center gap-2">
+          {periodIcons[period?.type]}
+          <h2 className="text-label-large-size text-content-primary">
+            {period?.title}
+          </h2>
+        </div>
+        <span className="text-label-large-size text-content-secondary">
+          {period.timeRange}
+        </span>
+      </div>
+    </section>
+  );
+};
